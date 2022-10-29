8314cb5ed21e12b7693662d7a6f583bfd27bb8b9
Prakash rai <rai530579@gmail.com>
2022-10-29 09:24:01 +0530

    .env file add

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-unused-vars */
 
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyCmtz54bM6jJK06HWrIeOtu1IV69aFBtLM",
-  authDomain: "crypto-hunter-bccfd.firebaseapp.com",
-  projectId: "crypto-hunter-bccfd",
-  storageBucket: "crypto-hunter-bccfd.appspot.com",
-  messagingSenderId: "678577520881",
-  appId: "1:678577520881:web:f3260b49c5732fbca86950",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 export default firebaseConfig;
